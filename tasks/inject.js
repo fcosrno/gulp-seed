4bc8d5a8b74ff724236eff0d760e90e8051ecb2f
Francisco Serrano <fserrano@gmail.com>
2016-02-05 11:44:17 -0500

    Simplify inject

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,9 +1,12 @@
 module.exports = function(gulp, plugins, config) {
-    gulp.task('inject',config.inject.runAfter, function() {
-        return gulp.src(config.inject.to)
-            .pipe(plugins.inject(gulp.src(config.inject.from, {
-                read: false
-            }), config.inject.options))
+    gulp.task('inject', config.inject.runAfter, function() {
+
+        var target = gulp.src(config.inject.to);
+        var sources = gulp.src(config.inject.from, {
+            read: false
+        });
+
+        return target.pipe(plugins.inject(sources, config.inject.options))
             .pipe(gulp.dest(config.inject.folder));
 
     });
