58ed2ff378ba58a5830e0cc530d201cd7bc9ff9c
fcosrno <fserrano@gmail.com>
2016-10-27 11:53:19 -0400

    Speed up gulp javascript task

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,9 +1,23 @@
 module.exports = function(gulp, plugins, config) {
-    gulp.task('javascript',config.javascript.runAfter,function() {
-      return gulp.src(config.javascript.from)
-          .pipe(plugins.concat(config.javascript.filename))
-          .pipe(plugins.uglify())
-          .pipe(plugins.rev())
-          .pipe(gulp.dest(config.javascript.to));
+
+    var argv = require('yargs').argv;
+
+    var environment = 'development';
+    if (argv.production) environment = 'production';
+    if (argv.staging) environment = 'staging';
+
+    gulp.task('javascript', config.javascript.runAfter, function() {
+        // Speed up development by skipping uglify and rev
+        if (environment === 'development') {
+            return gulp.src(config.javascript.from)
+                .pipe(plugins.concat(config.javascript.filename))
+                .pipe(gulp.dest(config.javascript.to));
+        } else {
+            return gulp.src(config.javascript.from)
+                .pipe(plugins.concat(config.javascript.filename))
+                .pipe(plugins.uglify())
+                .pipe(plugins.rev())
+                .pipe(gulp.dest(config.javascript.to));
+        }
     });
 };
