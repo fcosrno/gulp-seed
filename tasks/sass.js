bd907afc9bedfd831b2ce3d7b8345bb2c1f00511
Francisco Serrano <fserrano@gmail.com>
2017-07-31 14:58:24 -0400

    Upgrade sass

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,9 +1,28 @@
 module.exports = function(gulp, plugins, config) {
+    var argv = require('yargs').argv;
+
+    var environment = 'development';
+    if (argv.production) environment = 'production';
+    if (argv.staging) environment = 'staging';
+
+
     gulp.task('sass', config.sass.runAfter, function() {
-        return gulp.src(config.sass.from)
-            .pipe(plugins.sass(config.sass.options).on('error', plugins.sass.logError))
-            .pipe(plugins.autoprefixer('last 10 version'))
-            .pipe(plugins.rev())
-            .pipe(gulp.dest(config.sass.to));
+        if (environment === 'development') {
+
+            for (i = 0; i <= config.sass.from.length - 1; i++) {
+                gulp.src(config.sass.from[i])
+                    .pipe(plugins.sass().on('error', plugins.sass.logError))
+                    .pipe(gulp.dest(config.sass.to[i]));
+            }
+            return;
+        } else {
+            for (i = 0; i <= config.sass.from.length - 1; i++) {
+                gulp.src(config.sass.from[i])
+                    .pipe(plugins.sass(config.sass.options).on('error', plugins.sass.logError))
+                    .pipe(plugins.cleanCss())
+                    .pipe(gulp.dest(config.sass.to[i]));
+            }
+            return;
+        }
     });
 };
