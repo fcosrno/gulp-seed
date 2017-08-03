915ed301c7f6657d19abfe628f8aaf073206ed45
Francisco Serrano <fserrano@gmail.com>
2017-08-03 12:48:32 -0400

    Watch task update

--- tasks/browser-sync-watch.js
+++ tasks/browser-sync-watch.js
@@ -1,7 +1,7 @@
 module.exports = function(gulp, plugins, config) {
   gulp.task('browser-sync-watch', config.browserSyncWatch.runAfter, function() {
     plugins.watch(config.browserSyncWatch.files, function() {
-      return gulp.start(['build'],function() {
+      return gulp.start([config.browserSyncWatch.runTask],function() {
           plugins.browserSync.reload();
       });
     });
